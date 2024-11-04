5e720519e67cbeb2b2e029ecb245d263b0d32454
Joona Mölsä <2306075@edu.karelia.fi>
2024-11-04 23:22:29 +0200

    Add project files.

---
+++ App/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using AppLogger;
+namespace App
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Logger.Log("Hello from App using AppLogger!");
+        }
+    }
+}
